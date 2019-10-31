8f0a1a5da7467d49c2efdd85fe09c12b56a2f759
pxanias <34348674+pxanias@users.noreply.github.com>
2019-10-31 23:57:21 +0200

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+# my first repository
+name = str(input ("whats your name ? "))
+if name == "panos" :
+    print ("hello world")
+elif name == "no name" :
+
+    print ("there is no name for"+ name)
+else:
+    print ("there is no data")
+#it should give name with no space after so it can execute.
+age = int(input("give me your age now,"+name+":"))
+if age > 18 :
+ print("you are old")
+elif age == 18:
+    print ("good luck")
+else:
+    print ("you are young")
+
+print()
+
+print()
+
+print ("END OF CODE")
